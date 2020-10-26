28c5f82ae59d54177ebb39bbdc3b2c85e3363233
plpetkov-tech <plpetkov@tutanota.com>
2020-10-26 16:58:35 +0200

    full app working, now is time to setup ci/cd

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,30 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
 
-
-
-function App() {
-  return (
-    <Router>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <Route exact path="/" component={Fib}/>
-      <Route path="/otherpage" component={OtherPage}/>
-      <div>
-        <Route exact path="/" component={Fib}/>
-        <Route path="/otherpage" component={OtherPage}/>
-      </div>
-    </div>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to Fib-App</h1>
+            <h6>An app to calculate Fibonacci Numbers</h6>
+            <Link to={`/`}>Home</Link>
+            <Link to={`/otherpage`}>Other Page</Link>
+          </header>
+          <div className="mainContent">
+            <Route exact path={`/`} component={Fib} />
+            <Route path={`/otherpage`} component={OtherPage} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default () => {
     return (
         <div>
-            Im in some other page!
+            Im in some other page!<br></br>
             <Link to = "/">Go back home</Link>
         </div>
     );
